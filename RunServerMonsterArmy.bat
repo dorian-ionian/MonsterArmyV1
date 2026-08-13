49ef8b5b0f3f634461fde5b208236cf71f9d74b6
dorian-ionian <dorian-ionian@users.noreply.github.com>
2026-08-12 21:12:14 -0400

    Add dragon/ragdoll/gametype-override mutators to launcher

--- RunServerMonsterArmy.bat
+++ RunServerMonsterArmy.bat
@@ -1,5 +1,5 @@
 @echo off
 :10
-ucc server ONS-Torlan?game=MonsterArmyV1.MonsterArmyGame -ini=UT2004MonsterArmy.ini -log=MonsterArmy_server.log
+ucc server ONS-Torlan?game=MonsterArmyV1.MonsterArmyGame?Mutator=2009Dragonv2.MutDragon,MonsterRagdollOverrideV1.MutMonsterRagdollOverride,ServerBrowserGametypeOverrideV1.MutServerBrowserGametypeOverride -ini=UT2004MonsterArmy.ini -log=MonsterArmy_server.log
 copy MonsterArmy_server.log MonsterArmy_servercrash.log
 goto 10
